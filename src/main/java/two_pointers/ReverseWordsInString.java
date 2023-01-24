2b9869e45419dfa2fa1df61783ef5f90f5ea5923
E. Hannah <echannah631@gmail.com>
2023-01-24 00:04:07 -0600

    reversing words in a sentence challenge

---
+++ src/main/java/two_pointers/ReverseWordsInString.java
@@ -0,0 +1,31 @@
+package two_pointers;
+
+import java.util.Arrays;
+
+public class ReverseWordsInString {
+    public static void main(String[] args) {
+        String s = "Hello World!";
+        String reversedString = reverseWords(s);
+        System.out.println(reversedString);
+    }
+
+    static String reverseWords(String sentence) {
+        String[] reversedString = sentence.split(" ");
+        int leftPointer = 0;
+        int rightPointer = reversedString.length - 1;
+
+        while (leftPointer < rightPointer) {
+            String temp = reversedString[leftPointer];
+            reversedString[leftPointer] = reversedString[rightPointer];
+            reversedString[rightPointer] = temp;
+            leftPointer++;
+            rightPointer--;
+        }
+
+        StringBuilder result = new StringBuilder();
+        for (String s : reversedString) {
+            result.append(s).append(" ");
+        }
+        return result.toString();
+    }
+}
